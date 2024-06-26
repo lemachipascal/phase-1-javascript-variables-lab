494cf89ab897a2ba75c727df3348fc4f0b63f6b7
Lemachipascal-dev <lemachijo421@gmail.com>
2024-06-26 14:58:29 +0300

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood;
+mostProfitableNeighborhood = "Chelsea";
+
+
+let companyCeo;
+companyCeo = "Susan Smith";
+
